c40fd61abb5017beda8bc7b4da6c2a6da73def93
Dheeraj kumar <Howtotech000@gmail.com>
2022-11-15 17:11:24 +0530

    eval

--- src/Component/ProductListing.jsx
+++ src/Component/ProductListing.jsx
@@ -1,103 +1,92 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import api from "../api/api";
-import listingCss from './listingCss.css'
+import listingCss from "./listingCss.css";
 
 const ProductListing = () => {
   const [data, setdata] = useState([]);
   const [filter, setfilter] = useState("");
-  const [sort, setsort] = useState("")
+  const [sort, setsort] = useState("");
 
   const retData = async () => {
     const response = await api.get("/products?_page=1&_limit=5");
     return response.data;
   };
 
-
-
-  const delteProduct = async (id) =>{
+  const delteProduct = async (id) => {
     await api.delete(`/products/${id}`);
-    const newdatalist = data.filter((data)=>{
-        return data.id!==id;
+    const newdatalist = data.filter((data) => {
+      return data.id !== id;
     });
 
     setdata(newdatalist);
-    
+  };
+
+  //   Sorting and filter
+
+  const handlefilter = async () => {
+    return await api.get(`/products?gender=${filter}`).then((response) => {
+      setdata(response.data);
+      console.log(filter, response.data);
+    });
+  };
+
+  const  handlesort=  async (e)=> {
+    setsort(e);
+    return await api.get(`/products?_sort=price&_order=${sort}`).then((response) => {
+      setdata(response.data);
+      console.log(filter, response.data);
+    });
   }
 
-
-//   Sorting and filter
-
-const  handlefilter= async (e)=>{
-    setfilter(e)
-
-   return await api.get(`/products?gender=${filter}`)
-   .then((response)=>{
-    setdata(response.data)
-    console.log(filter, response.data)
-   })
-
-}
-
-function handlesort(e){
-    setsort(e)
-}
-
-//   Sorting and filter end
-
-
+  //   Sorting and filter end
 
   useEffect(() => {
     const getAlldata = async () => {
       const alldata = await retData();
       setdata(alldata);
-      
     };
     getAlldata();
-
   }, []);
-
-
 
   return (
     <div>
-        <div className="btnpage">
-            
+      <div className="btnpage">
         <select
-        onChange={e=>handlefilter(e.target.value)}
-        value={filter}
+          onChange={(e) => {
+            setfilter(e.target.value);
+            handlefilter();
+          }}
+          value={filter}
         >
-            <option value="filter">Filter</option>
-            <option value="male">female</option>
-            <option value="female">male</option>
+          <option value="filter">Filter</option>
+          <option value="female">male</option>
+          <option value="male">female</option>
         </select>
-        <select
-        onChange={e=>handlesort(e.target.value)}
-        value={sort}
-        >
-            <option value="sort">Sort</option>
-            <option value="low">Low To High</option>
-            <option value="high">High To Low</option>
+
+        <select onChange={(e) => handlesort(e.target.value)} value={sort}>
+          <option value="sort">Sort</option>
+          <option value="desc">Low To High</option>
+          <option value="asc">High To Low</option>
         </select>
-        </div>
+      </div>
       {data.map((e) => {
         return (
-         
-            <div id="Productcard" key={e.id}>
-              <p className="title">{e.title}</p>
-              <img  className="img" src={e.image} alt="" />
-              <p className="price">Price: {e.price}</p>
-              <p className="category"> Category: {e.category}</p>
-              <p className="gender">Gender :{e.gender}</p>
-              <button className="delbtn" onClick={()=>delteProduct(e.id)} >Delete Product</button>
-            </div>
-          
+          <div id="Productcard" key={e.id}>
+            <p className="title">{e.title}</p>
+            <img className="img" src={e.image} alt="" />
+            <p className="price">Price: {e.price}</p>
+            <p className="category"> Category: {e.category}</p>
+            <p className="gender">Gender :{e.gender}</p>
+            <button className="delbtn" onClick={() => delteProduct(e.id)}>
+              Delete Product
+            </button>
+          </div>
         );
       })}
       <div className="btnpage">
-
-      <button>Prev</button>
-      <button>Next</button>
+        <button>Prev</button>
+        <button>Next</button>
       </div>
     </div>
   );
